c8fb172be2148bd44a76f387761fbe1e5b42147f
AMadayag <hiding303@gmail.com>
2025-04-01 23:10:00 +1100

    finished loginUser tests

--- Tests/InvoiceGeneration(Sushi)/loginUser.test.ts
+++ Tests/InvoiceGeneration(Sushi)/loginUser.test.ts
@@ -1,7 +1,5 @@
 import { loginUserReq } from "../testHelper";
 
-// test for registerUser
-// test is skipped because user is already created
 describe('loginUser', () => {
   test('successful login', async () => {
     const res = await loginUserReq(
@@ -16,6 +14,25 @@ describe('loginUser', () => {
       await loginUserReq(
         'user@gmail.com',
         'wrongpwd'
+      );
+      fail('Did not throw expected error');
+    } catch (error) {
+      if (error instanceof Error) {
+        const axiosError = error as any;
+        expect(axiosError.response.status).toBe(400);
+        expect(axiosError.response.data.status).toStrictEqual(expect.any(String));
+        expect(axiosError.response.data.message).toStrictEqual(expect.any(String));
+      } else {
+        throw error;
+      }
+    }
+  });
+
+  test('incorrect user', async () => {
+    try {
+      await loginUserReq(
+        'incorrectuser@gmail.com',
+        'passworD123!'
       );
       fail('Did not throw expected error');
     } catch (error) {
