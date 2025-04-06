ee91f4583c28b0087aee450166949b771442a7fc
AMadayag <hiding303@gmail.com>
2025-04-06 20:13:48 +1000

    added some tests for delete

--- Tests/DespatchAdvice(iOS)/getAll.test.ts
+++ Tests/DespatchAdvice(iOS)/getAll.test.ts
@@ -5,25 +5,5 @@ describe('get all despatch ids', () => {
     const res = await getAllReq();
     expect(res.status).toBe(200);
     console.log(res.data);
-  })
-
-  // test('invalid email', async () => {
-  //   try {
-  //     await registerUserReq(
-  //       '',
-  //       'company',
-  //       'passworD123!'
-  //     );
-  //     fail('Did not throw expected error');
-  //   } catch (error) {
-  //     if (error instanceof Error) {
-  //       const axiosError = error as any;
-  //       expect(axiosError.response.status).toBe(400);
-  //       expect(axiosError.response.data.status).toStrictEqual(expect.any(String));
-  //       expect(axiosError.response.data.message).toStrictEqual(expect.any(String));
-  //     } else {
-  //       throw error;
-  //     }
-  //   }
-  // });
+  });
 });
